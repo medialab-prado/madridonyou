aa7318e688b5ccbe175109dfcaf23db3363e7f0b
Jesús Pérez <jesusperezmelero@gmail.com>
2017-10-21 16:24:59 +0200

    Añadiendo contenido realizado hasta la fecha

---
+++ implementacion/front/src/main/java/com/madridonyou/micro/front/Launcher.java
@@ -0,0 +1,13 @@
+package com.madridonyou.micro.front;
+
+/**
+ * Hello world!
+ *
+ */
+public class Launcher 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ implementacion/front/src/main/java/com/madridonyou/micro/front/api/FrontAPI.java
@@ -0,0 +1,8 @@
+package com.madridonyou.micro.front.api;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public interface FrontAPI {
+
+}
